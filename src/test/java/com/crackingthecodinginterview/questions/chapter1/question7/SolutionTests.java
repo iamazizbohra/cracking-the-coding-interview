e485fee4641e33ec83bd63b5eea62b8d3bfad675
Aziz Bohra <imazizbohra@gmail.com>
2023-12-18 17:11:02 +0530

    update test

--- src/test/java/com/crackingthecodinginterview/questions/chapter1/question7/SolutionTests.java
+++ src/test/java/com/crackingthecodinginterview/questions/chapter1/question7/SolutionTests.java
@@ -1,6 +1,7 @@
 package com.crackingthecodinginterview.questions.chapter1.question7;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.DisplayName;
@@ -29,6 +30,23 @@ public class SolutionTests {
 				() -> assertThat(expectedMatrix[1]).containsExactly(14, 10, 6, 2),
 				() -> assertThat(expectedMatrix[2]).containsExactly(15, 11, 7, 3),
 				() -> assertThat(expectedMatrix[3]).containsExactly(16, 12, 8, 4));
+	}
+
+	@Test
+	@DisplayName("Rotate uneven matrix by 90 degree test")
+	public void givenMatrix_whenRotate_thenReturnMatrix() {
+		// given
+		int[][] actualMatrix = {
+				{ 1, 2, 3, 4 },
+				{ 5, 6, 7, 8 }
+		};
+
+		// when
+		Solution obj = new Solution();
+		Throwable thrown = catchThrowable(() -> obj.rotate(actualMatrix));
+
+		// then
+		assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
 	}
 
 }
